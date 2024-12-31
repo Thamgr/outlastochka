8992274c49c6d227b3dc64ec543723a5dbf229ee
Thamgr <thamgr@thamgr0.ru-central1.internal>
2024-12-31 22:16:25 +0000

    read token from file

--- const.py
+++ const.py
@@ -7,8 +7,13 @@ DAY = 24 * HOUR
 WEEK = 7 * DAY
 EPS_TIME = 0.01 * SECOND
 
+kConfPath = './data/main.conf'
+kTokenPath = './data/secret'
+
 chat_id = -1002446099140
 token = ''
+with open(kTokenPath) as file:
+    config = file.readline()
 
 kIvanovo2Moscow = {
     'title': '{} | Иваново -> Москва',
@@ -18,4 +23,3 @@ kMoscow2Ivanovo = {
     'title': '{} | Москва -> Иваново',
     'options': ['01:56', '07:07', '14:01', '20:07', 'Тык'],
 }
-kConfPath = './data/main.conf'
